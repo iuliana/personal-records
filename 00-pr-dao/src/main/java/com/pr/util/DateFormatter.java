3e258bbe2569d1520bf79f81a29b637923026269
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2017-10-14 09:59:33 +0300

    Switched to Gradle 4.2.1, updating sources to remove usage of deprecated classes

--- 00-pr-dao/src/main/java/com/pr/util/DateFormatter.java
+++ 00-pr-dao/src/main/java/com/pr/util/DateFormatter.java
@@ -1,8 +1,13 @@
 package com.pr.util;
 
 import org.springframework.format.Formatter;
+
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
@@ -10,17 +15,19 @@ import java.util.Locale;
  * @author Iuliana Cosmina.
  */
 public class DateFormatter implements Formatter<Date> {
-    public static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
 
     @Override
     public Date parse(String s, Locale locale) throws ParseException {
-        return formatter.parse(s);
+        LocalDateTime localDateTime = LocalDate.from(FORMATTER.parse(s)).atStartOfDay();
+        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+        return Date.from(instant);
     }
 
     @Override
     public String print(Date date, Locale locale) {
-        return formatter.format(date);
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        return FORMATTER.format(localDateTime);
     }
-
 }
-
--- 04-pr-mvc-view-solution/src/main/java/com/pr/views/PersonsExcelView.java
+++ 04-pr-mvc-view-solution/src/main/java/com/pr/views/PersonsExcelView.java
@@ -1,13 +1,16 @@
 package com.pr.views;
 
 import com.pr.ents.Person;
-import org.apache.poi.hssf.usermodel.*;
+import com.pr.util.DateFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.view.document.AbstractExcelView;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -15,39 +18,21 @@ import java.util.Map;
  * Created by iuliana.cosmina on 3/20/15.
  */
 @Component("persons/list.xls")
-public class PersonsExcelView extends AbstractExcelView {
-    @Override
-    protected void buildExcelDocument(Map<String, Object> model, HSSFWorkbook workbook, HttpServletRequest request,
-                                      HttpServletResponse response) throws Exception {
+public class PersonsExcelView extends AbstractXlsView {
 
-        List<Person> accounts = (List<Person>) model.get("persons");
-
-        HSSFCellStyle dateStyle = workbook.createCellStyle();
-        dateStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("yyyy-MM-dd"));
-
-        HSSFSheet sheet = workbook.createSheet();
-
-        for (short i = 0; i < accounts.size(); i++) {
-            Person person = accounts.get(i);
-            HSSFRow row = sheet.createRow(i);
-            addStringCell(row, 0, person.getFirstName());
-            addStringCell(row, 1, person.getLastName());
-            addDateCell(row, 2, person.getDateOfBirth(), dateStyle);
-        }
-
-    }
-
-    private HSSFCell addStringCell(HSSFRow row, int index, String value) {
-        HSSFCell cell = row.createCell(index);
-        cell.setCellValue(new HSSFRichTextString(value));
-        return cell;
-    }
-
-    private HSSFCell addDateCell(HSSFRow row, int index, Date date,
-                                 HSSFCellStyle dateStyle) {
-        HSSFCell cell = row.createCell(index);
-        cell.setCellValue(date);
-        cell.setCellStyle(dateStyle);
-        return cell;
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		List<Person> accounts = (List<Person>) model.get("persons");
+		Sheet sheet = workbook.createSheet();
+		for (short i = 0; i < accounts.size(); i++) {
+			Person person = accounts.get(i);
+			Row row = sheet.createRow(i);
+			row.createCell(0).setCellValue(person.getFirstName());
+			row.createCell(1).setCellValue(person.getLastName());
+			DateFormatter df = new DateFormatter();
+			row.createCell(2).setCellValue(df.print(person.getDateOfBirth(), LocaleContextHolder.getLocale()));
+		}
+	}
 }
